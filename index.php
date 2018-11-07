07ade8bf6c83992b4eab691e5cdbe087211cb226
Daniel <daniel.rod.vega@hotmail.com>
2018-11-07 11:14:11 -0700

    Post Login
    Post Login

--- index.php
+++ index.php
@@ -16,15 +16,34 @@ and open the template in the editor.
     <body>
         <div id="contenedor">
             <div id="login">
-                <img id="logo" src="Resources/bepclogo.png">
+                <div id="logoAndInfo">
+                    <div id="imagenLogo">
+                        <img src="Resources/bee logo.png" id="beeLogo">
+                    </div>
+                    <div id="nombre">
+                        <span style="color:  rgb(239, 198, 18); 
+                              text-shadow:  1px 0 rgb(239, 198, 18), 0 -1px rgb(239, 198, 18);">BE</span><span style="color:  rgb(49, 63, 94);">TRACKING</span>
+                    </div>
+                </div>
                 <form id="formulario" onsubmit="return Login();">
-                    <div class="info"><i class="fas fa-user"></i>   &nbsp;&nbsp;USERNAME</div>
+                    <div class="info">&nbsp;&nbsp;Nombre de Usuario</div>
                     <div class="input"><input type="text" name="username" id="usuario" required></div>
-                    <div class="info"><i class="fas fa-lock"></i>   &nbsp;&nbsp;PASSWORD</div>
+                    <div class="info">&nbsp;&nbsp;Contraseña</div>
                     <div class="input"><input type="password" name="password" id="password" required></div>
-                    <div class="input"><input type="submit" value="LOGIN"></div>
+                    <div class="input"><input type="submit" value="INICIAR SESION"></div>
+                    <div id="recordar"><input type="checkbox" style="float:left; width: 15px; margin-top: -6px;">Recordarme</div>
+                    <div id="recuperar" onclick="mostrarRecuperar();">¿Olvidaste tu contraseña?</div>
                 </form>
-                <label id="recuperar" onclick="mostrarRecuperar();">Forgot your password? ></label>
+            </div>
+            <div id="information">
+                <div class="hexagon">
+                    <p>
+                        En el Cuadrante Mágico de Gartner, se nombró a Salesforce como 
+                        líder debido al centro de interacción con el cliente de CRM por 10 años consecutivos
+                        <br>
+                    </p>
+                        <button id="conoceMas">CONOCE MÁS</button>
+                </div>
             </div>
             <div id="forgot">
                 <form id="enviarRecuperar" onsubmit="return EnviarPassword();">
